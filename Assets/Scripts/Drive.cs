690f6ea934f1271292f1b95331580d759d04d17f
Fatih <fatih.altuntas@noho.games>
2021-05-26 09:51:33 +0300

    created basic 3D chase system. - started to 2D auto pilot chase system. created basic movement script for 2D tank.

---
+++ Assets/Scripts/Drive.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Drive : MonoBehaviour
+{
+    public float speed = 10.0f;
+    public float rotationSpeed = 100.0f;
+
+    private void Update()
+    {
+        float translation = Input.GetAxis("Vertical");
+        float rotation = Input.GetAxis("Horizontal");
+
+        transform.Translate(0, translation * speed * Time.deltaTime, 0);
+        transform.Rotate(0, 0, -rotation * rotationSpeed * Time.deltaTime);
+    }
+}
---
+++ Assets/Scripts/Move.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using UnityEngine;
+
+public class Move : MonoBehaviour
+{
+    public Transform goal;
+    public float speed = 1f;
+    public float accuracy = 0.01f;
+
+    private void LateUpdate()
+    {
+        transform.LookAt(goal.position);
+        
+        Vector3 direction = goal.transform.position - transform.position;
+        Debug.DrawRay(transform.position, direction, Color.red);
+        if (direction.magnitude > accuracy)
+        {
+            transform.Translate(direction.normalized * speed * Time.deltaTime, Space.World);
+        }
+    }
+}
