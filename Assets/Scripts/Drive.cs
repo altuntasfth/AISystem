d760810efa3f73332276e6759be4de22cc8dbf56
Fatih <fatih.altuntas@noho.games>
2021-05-28 14:53:00 +0300

    calculated manuel distance and result is compare by Vector3.Distance calculation. (hypotenuse)

--- Assets/Scripts/Drive.cs
+++ Assets/Scripts/Drive.cs
@@ -6,6 +6,7 @@ public class Drive : MonoBehaviour
 {
     public float speed = 10.0f;
     public float rotationSpeed = 100.0f;
+    public Transform fuel;
 
     private void Update()
     {
@@ -14,5 +15,24 @@ public class Drive : MonoBehaviour
 
         transform.Translate(0, translation * speed * Time.deltaTime, 0);
         transform.Rotate(0, 0, -rotation * rotationSpeed * Time.deltaTime);
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            CalculateDistance();
+        }
+    }
+
+    private void CalculateDistance()
+    {
+        Vector3 tankPos = transform.position;
+        Vector3 fuelPos = fuel.position;
+
+        float distance = Mathf.Sqrt(Mathf.Pow(tankPos.x - fuelPos.x, 2) +
+                                    Mathf.Pow(tankPos.y - fuelPos.y, 2) +
+                                    Mathf.Pow(tankPos.z + fuelPos.z, 2));
+        float unityDistance = Vector3.Distance(tankPos, fuelPos);
+        
+        Debug.Log("Calculated distance : " + distance);
+        Debug.Log("Unity distance: " + unityDistance);
     }
 }
