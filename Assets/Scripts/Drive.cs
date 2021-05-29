a8ab7eae08608cb9709213b41afc4dd6ce626b93
Fatih <fatih.altuntas@noho.games>
2021-05-29 15:59:20 +0300

    added 3D Tank asset.

---
+++ Assets/Scripts/Drive.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Drive : MonoBehaviour
+{
+    public float speed = 10.0f;
+    public float rotationSpeed = 100.0f;
+
+    private void Update()
+    {
+        float translation = Input.GetAxis("Vertical");
+        float rotation = Input.GetAxis("Horizontal");
+
+        transform.Translate(0, 0, translation * speed * Time.deltaTime);
+        transform.Rotate(0, rotation * rotationSpeed * Time.deltaTime, 0);
+    }
+}
---
+++ Assets/Scripts/Shell.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shell : MonoBehaviour
+{
+    public GameObject explosion;
+
+    private void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.tag == "tank")
+        {
+            GameObject exp = Instantiate(explosion, this.transform.position, Quaternion.identity);
+            Destroy(exp, 0.5f);
+            Destroy(this.gameObject);
+        }
+    }
+}
